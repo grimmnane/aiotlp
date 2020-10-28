6135a114c632598f4a44cbb18189487ccd452d5e
Faiz <18261880837@163.com>
2020-10-28 23:13:29 +0800

    传感器页面动态化

--- pages/sensorChart/index.js
+++ pages/sensorChart/index.js
@@ -1,6 +1,6 @@
 // pages/sensorChart/index.js
 import * as echarts from '../../ec-canvas/echarts';
-
+let chart = null;
 
 Page({
 
@@ -8,16 +8,35 @@ Page({
    * 页面的初始数据
    */
   data: {
+    active: 0, // tab active
     ec: {
       onInit: initChart
-    }
+    },
+    show: false, // 所属区域弹框
+    columns: [], // 所属区域picker
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 根据参数请求接口
+    // wx.request({
+    //   url: 'url',
+    //   success: ()=>{
 
+    //   },
+    // })
+
+
+    this.setData({
+      columns: ['鱼塘', '养殖场']
+    });
+    let x = ['1时', '2时', '3时', '4时', '5时', '6时', '7时', '8时', '9时', '10时'];
+    let data = [123, 789, 159, 753, 654, 240, 954, 180, 264, 650];
+    setTimeout(()=>{
+      updateChart(x,data);
+    },1500)
   },
 
   /**
@@ -69,11 +88,49 @@ Page({
 
   },
 
+  // tab切换日期
+  onChange(event) {
+    if(event.detail.name == 0){
+      // 请求当天数据
+
+      let x = ['1时', '2时', '3时', '4时', '5时', '6时', '7时', '8时', '9时', '10时'];
+      let data = [123, 789, 159, 753, 654, 240, 954, 180, 264, 650];
+      updateChart(x, data);
+    }
+    if(event.detail.name == 1){
+      // 请求7天数据
+
+      let x = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
+      let data = [159, 123, 248, 368, 754, 456, 369];
+      updateChart(x, data);
+    }
+    if(event.detail.name == 2){
+      // 请求30天数据
+
+      let x = ['1号', '3号', '5号', '7号', '9号', '11号', '13号', '15号'];
+      let data = [147, 258, 369, 159, 456, 789, 652, 149];
+      updateChart(x, data);
+    }
+  },
+
+  // 切换所属区域
+  onAreaChange(event) {
+    // const { picker, value, index } = event.detail;
+    // Toast(`当前值：${value}, 当前索引：${index}`);
+  },
+  // 所属区域弹窗
+  showPopup() {
+    this.setData({ show: true });
+  },
+  onClose() {
+    this.setData({ show: false });
+  },
+
 
 })
 
 function initChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
+  chart = echarts.init(canvas, null, {
     width: width,
     height: height,
     devicePixelRatio: dpr // new
@@ -81,7 +138,7 @@ function initChart(canvas, width, height, dpr) {
   canvas.setChart(chart);
 
   var option = {
-    color: "#2E8B57", // 线条颜色
+    color: "#71BB6E", // 线条颜色
     grid: {
       containLabel: true // false用于多个grid对齐，true用于防止标签溢出
     },
@@ -106,10 +163,28 @@ function initChart(canvas, width, height, dpr) {
       name: 'ppm',
       type: 'line',
       smooth: true,
-      data: [180, 360, 650, 300, 780, 400, 330, 180, 360, 650]
+      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     }]
   };
 
   chart.setOption(option);
   return chart;
+}
+
+
+function updateChart(xAxis,series){
+  var option = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: xAxis,
+    },
+    series: [{
+      name: 'ppm',
+      type: 'line',
+      smooth: true,
+      data: series
+    }]
+  }
+  chart.setOption(option);
 }
