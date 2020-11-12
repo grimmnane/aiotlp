7fad5d37b858831b9c44d22115f2c5a1f572d3b7
Faiz <18261880837@163.com>
2020-11-12 22:44:22 +0800

    分享

--- pages/share/bind.js
+++ pages/share/bind.js
@@ -1,29 +1,31 @@
 // pages/share/bind.js
-Page({
+const app = getApp()
+const p = require('../../utils/promission')
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    data: null, // 分享携带数据
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    let data = JSON.parse(options.data);
-    // console.log(data);
-    this.setData({
-      data: data
-    });
-  },
-
-
-  // 绑定
-  tologin(){
-    wx.navigateTo({
-      url: '/pages/login/login/index',
-    })
-  }
-})
+Page(p.promission({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      data: null, // 分享携带数据
+    },
+  
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+      // let data = JSON.parse(options.data);
+      console.log(options);
+      console.log(app.globalData.token);
+      this.setData({
+        data: 'data'
+      });
+    },
+  
+  
+    // 绑定
+    tologin(){
+      console.log(app.globalData.token);
+      console.log(this.data.data);
+    }
+}))
