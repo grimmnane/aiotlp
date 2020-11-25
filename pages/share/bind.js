bd13151b5d84e0efcc0cbb8f54e5aa02bf01142e
tracy-yang <prayer_m@sina.cn>
2020-11-25 21:53:31 +0800

    修改绑定页

--- pages/share/bind.js
+++ pages/share/bind.js
@@ -15,6 +15,7 @@ Page({
     name:'', // 昵称
     deviceNames:'' , // 传过来的传感器名称
     show: false, // 是否显示隐私页
+    shareDeviceList:[] , // 分享设备列表
   },
 
   /**
@@ -37,24 +38,43 @@ Page({
     this.data.shareKey = wx.getStorageSync('shareKey');
     if(!this.data.shareKey){
       this.data.shareKey = options.shareKey || '';
+      this.getShareDeviceInfo();
       wx.setStorageSync('shareKey',this.data.shareKey)
     }
-    this.data.name = wx.getStorageSync('shareName');
-    if(!this.data.name){
-      this.data.name  = options.name || '';
-      wx.setStorageSync('shareName', this.data.name);
-    }
-    this.data.deviceNames = wx.getStorageSync('deviceNames');
-    if(!this.data.deviceNames){
-      this.data.deviceNames  = options.deviceNames || '';
-      wx.setStorageSync('deviceNames',this.data.deviceNames);
-    }
-    this.setData({
-      name: this.data.name,
-      deviceNames: this.data.deviceNames
+    // this.data.name = wx.getStorageSync('shareName');
+    // if(!this.data.name){
+    //   this.data.name  = options.name || '';
+    //   wx.setStorageSync('shareName', this.data.name);
+    // }
+    // this.data.deviceNames = wx.getStorageSync('deviceNames');
+    // if(!this.data.deviceNames){
+    //   this.data.deviceNames  = options.deviceNames || '';
+    //   wx.setStorageSync('deviceNames',this.data.deviceNames);
+    // }
+  },
+
+  // 获取分享设备信息
+  getShareDeviceInfo(){
+    if(!this.data.shareKey) return;
+    util.request('/sensor/web-share/shareDeviceList',{method:'GET',header:{'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',},data:{shareKey:this.data.shareKey}}).then(res =>{
+      let list = this.setShareDeviceKey(res.data || []);
+      this.setData({shareDeviceList:list})
+    }).catch(()=>{
+
     })
   },
 
+  setShareDeviceKey(list){
+    return list.map(item =>{
+      item.deviceId = item['设备ID'] || '';
+      item.deviceName = item['设备名'] || '';
+      item.img = item['设备图标'] || '';
+      return item;
+    })
+  },
+
+
+  // 验证
   validate(){
     if(this.data.show){ // 没有登陆
       this.tobind();
@@ -72,8 +92,8 @@ Page({
     util.request('/sensor/web-share/bindShare',{method:'POST',data:{shareKey:this.data.shareKey}}).then(res =>{
       Toast.clear();
       wx.removeStorageSync('shareKey');
-      wx.removeStorageSync('deviceNames');
-      wx.removeStorageSync('shareName');
+      // wx.removeStorageSync('deviceNames');
+      // wx.removeStorageSync('shareName');
       Dialog.alert({
         message: res.message || '绑定成功',
       }).then(() => {
@@ -82,8 +102,8 @@ Page({
     }).catch(()=>{
       Toast.clear();
       wx.removeStorageSync('shareKey');
-      wx.removeStorageSync('deviceNames');
-      wx.removeStorageSync('shareName');
+      // wx.removeStorageSync('deviceNames');
+      // wx.removeStorageSync('shareName');
     })
   },
 
